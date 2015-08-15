dceca61f3a54e59406b9c8d9c8c55dc3561a1463
Jerry Zou <zry656565@gmail.com>
2015-08-16 00:31:50 +0800

    fixup! add a new solution for [9: Palindrome Number]

---
+++ code/js/palindrome_number.js
@@ -0,0 +1,22 @@
+/****
+ * Solution of https://leetcode.com/problems/palindrome-number/
+ * @author: Jerry Zou
+ * @date: 2015.8.16
+ **/
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    var str = x.toString(),
+        len = Math.floor(str.length / 2)
+
+    for (var i = 0; i < len; i++ ) {
+        if (str[i] != str[str.length - i - 1]) {
+            return false
+        }
+    }
+
+    return true
+}
